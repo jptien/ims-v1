b196ca8b48137d7bf26e340f2b007c1ba033eb1c
tieuhao2003 <tieuhao2003@adb48daa-390e-e48d-94fb-b6df6e74a28b>
2010-04-10 02:20:58 +0000

    Starting Project !!

---
+++ trunk/ ims-v1/mjsip_1.6/src/org/zoolu/sip/header/PAccessNetworkInfoHeader.java
@@ -0,0 +1,26 @@
+package org.zoolu.sip.header;
+
+
+/**
+ * @author TuanHao
+ * Support IMS 
+ */
+public class PAccessNetworkInfoHeader extends PHeader {
+
+	protected static String P_INFO= "3GPP-UTRAN-TDD; utran-cell-id-3gpp=234151D0FCE11";
+	
+	public PAccessNetworkInfoHeader() {
+		super(SipHeaders.P_Access_Network_Info, P_INFO);
+	}
+	public PAccessNetworkInfoHeader(String hname, String hvalue) {
+		super(hname,hvalue);
+	}
+	
+	public PAccessNetworkInfoHeader(String hvalue) {
+		super(SipHeaders.P_Access_Network_Info, hvalue);
+	}
+
+	public PAccessNetworkInfoHeader(Header hd) {
+		super(hd);
+	}
+}
